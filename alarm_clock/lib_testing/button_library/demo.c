79fc44d4e6feb77c76d18f484a072213914efac2
SImon QUe <ques86225@gmail.com>
2026-07-04 13:59:07 +0800

    separated the libraries testing and src directory for the project Testing the libraries with demos is now separate from the main alarm clock include directory. todo: Alarm passive buzzer library

---
+++ alarm_clock/lib_testing/button_library/demo.c
@@ -0,0 +1,163 @@
+#include "src/button.h"
+#include "src/gpio.h"
+#include "src/millis_micros.h"
+
+Pin segment_pins[8];
+Pin common_pin;
+Button btns[3];
+// Pin btn_pin;
+/*
+IO pins
+ */
+#define LEDS_PORT PORTD
+#define LEDS_DDR DDRD
+#define LED_1_PIN_NUM PD0
+#define LED_2_PIN_NUM PD1
+#define LED_3_PIN_NUM PD2
+
+#define COMMON_LED_PORT PORTB
+#define COMMON_LED_PIN PB0
+#define COMMON_LED_DDR DDRB
+
+#define BTNS_PORT PORTC
+#define BTNS_DDR DDRC
+#define BTNS_PIN PINC
+#define BTN_1_PIN_NUM PC1 // down
+#define BTN_2_PIN_NUM PC2 // reset
+#define BTN_3_PIN_NUM PC3 // up
+
+#define seven_seg_port PORTB
+
+const uint8_t digit_values[16] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111, // 9
+    0b01110111, // A
+    0b01111100, // b
+    0b00111001, // C
+    0b01011110, // d
+    0b01111001, // E
+    0b01110001, // F
+};
+int8_t cur_digit;
+
+ISR(TIMER0_COMPA_vect)
+{
+    millis_timer_ISR_loop();
+}
+
+void ioinit()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        segment_pins[i].ddr = &LEDS_DDR;
+        segment_pins[i].port = &LEDS_PORT;
+        segment_pins[i].pin_num = i;
+        gpio_set_pin_output(&segment_pins[i]);
+    }
+
+    common_pin.ddr = &COMMON_LED_DDR;
+    common_pin.port = &COMMON_LED_PORT;
+    common_pin.pin_num = COMMON_LED_PIN;
+
+    btns[0].button_pin.ddr = &BTNS_DDR;
+    btns[0].button_pin.port = &BTNS_PORT;
+    btns[0].button_pin.pin = &BTNS_PIN;
+    btns[0].button_pin.pin_num = BTN_1_PIN_NUM;
+    btns[1].button_pin.ddr = &BTNS_DDR;
+    btns[1].button_pin.port = &BTNS_PORT;
+    btns[1].button_pin.pin = &BTNS_PIN;
+    btns[1].button_pin.pin_num = BTN_2_PIN_NUM;
+    btns[2].button_pin.ddr = &BTNS_DDR;
+    btns[2].button_pin.port = &BTNS_PORT;
+    btns[2].button_pin.pin = &BTNS_PIN;
+    btns[2].button_pin.pin_num = BTN_3_PIN_NUM;
+
+    gpio_set_pin_output(&segment_pins[0]);
+    gpio_set_pin_output(&segment_pins[1]);
+    gpio_set_pin_output(&segment_pins[2]);
+    gpio_set_pin_output(&common_pin);
+    button_setup(&btns[0]);
+    button_setup(&btns[1]);
+    button_setup(&btns[2]);
+    // gpio_set_pin_input(&btn_pin);
+    // gpio_set_pin_high(&btn_pin);
+
+    gpio_set_pin_low(&common_pin);
+}
+
+void btn1_1click_func()
+{
+    // gpio_toggle_pin_output(&segment_pins[0]);
+    cur_digit--;
+    if (cur_digit < 0)
+    {
+        cur_digit = 15;
+    }
+}
+
+void btn2_1click_func()
+{
+    // gpio_toggle_pin_output(&segment_pins[1]);
+    cur_digit = 0;
+}
+
+void btn3_1click_func()
+{
+    // gpio_toggle_pin_output(&segment_pins[2]);
+    cur_digit++;
+    if (cur_digit > 15)
+    {
+        cur_digit = 0;
+    }
+}
+
+void f1()
+{
+    gpio_set_pin_high(&common_pin);
+}
+
+void f2()
+{
+    gpio_set_pin_low(&common_pin);
+}
+
+uint8_t reading;
+
+int main(void)
+{
+    ioinit();
+    millis_timer_init();
+    button_attach_single_click(&btns[0], btn1_1click_func);
+    button_attach_single_click(&btns[1], btn2_1click_func);
+    button_attach_single_click(&btns[2], btn3_1click_func);
+    button_attach_double_click(&btns[0], btn3_1click_func);
+    button_attach_long_press(&btns[0], btn2_1click_func);
+
+    while (1)
+    {
+        // reading = gpio_read_pin(&btn_pin);
+        // // reading = 1;
+        // if (reading)
+        // {
+        //     gpio_set_pin_high(&common_pin);
+        // }
+        // else
+        // {
+        //     gpio_set_pin_low(&common_pin);
+        // }
+        button_ISR_tick(&btns[0]);
+        button_ISR_tick(&btns[1]);
+        button_ISR_tick(&btns[2]);
+        // button_debounce(&btns[0], f1, f2);
+        //)
+        PORTD = ~digit_values[cur_digit];
+    }
+}
---
+++ alarm_clock/lib_testing/seven_segment_display_library/src/seven_segment.c
@@ -0,0 +1,282 @@
+#include "seven_segment.h"
+
+const uint8_t digit_values[17] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111, // 9
+    0b01110111, // A
+    0b01111100, // b
+    0b00111001, // C
+    0b01011110, // d
+    0b01111001, // E
+    0b01110001, // F
+    0b01110100, // h (for humidity)
+};
+
+display_data_t display_data;
+Pin segment_pins[8];
+Pin digit_pins[4];
+Pin colon_pin;
+
+void seven_segment_init()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        segment_pins[i].ddr = &SEGMENTS_DDR;
+        segment_pins[i].port = &SEGMENTS_PORT;
+        segment_pins[i].pin_num = i;
+        gpio_set_pin_output(&segment_pins[i]);
+    }
+    for (int i = DIGITS_START_INDEX; i < 4; i++)
+    {
+        digit_pins[i].ddr = &DIGITS_DDR;
+        digit_pins[i].port = &DIGITS_PORT;
+        // digit_pins[i].pin = &PINB;
+        digit_pins[i].pin_num = i;
+        gpio_set_pin_output(&digit_pins[i]);
+    }
+    colon_pin.ddr = &COLON_DDR;
+    colon_pin.port = &COLON_PORT;
+    colon_pin.pin_num = COLON_PIN_NUM;
+    gpio_set_pin_output(&colon_pin);
+}
+
+void seven_segment_loop_isr(void)
+{
+    gpio_set_pin_val(&colon_pin, display_data.colon);
+
+    DIGITS_PORT |= 0x0F;
+    // PORTB &= ~0x08;
+    DIGITS_PORT &= ~_BV(3 - display_data.cur_digit_index);
+    SEGMENTS_PORT = ~display_data.display_digits[display_data.cur_digit_index];
+    display_data.cur_digit_index++;
+    if (display_data.cur_digit_index > 3)
+    {
+        display_data.cur_digit_index = 0;
+    }
+}
+
+/**
+ * @brief
+ *
+ * @param
+ * @return
+ */
+void seven_segment_clear_all(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        display_data.display_digits[i] = 0;
+    }
+    display_data.colon = 0;
+}
+
+void seven_segment_set_all(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        display_data.display_digits[i] = 255;
+    }
+    display_data.colon = 1;
+}
+
+void seven_segment_write_byte_to_digit(uint8_t byte, uint8_t digit)
+{
+    display_data.display_digits[digit] = byte;
+}
+
+void seven_segment_write_bytes(uint8_t bytes[4])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        seven_segment_write_byte_to_digit(bytes[i], i);
+    }
+}
+
+void seven_segment_write_digit_val_to_digit(uint8_t digit_index, uint8_t digit)
+{
+    seven_segment_write_byte_to_digit(digit_values[digit_index], digit);
+}
+
+void seven_segment_write_digit_vals(uint8_t digit_indices[4])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        seven_segment_write_digit_val_to_digit(digit_indices[i], i);
+    }
+}
+
+void seven_segment_set_colon(uint8_t state)
+{
+    display_data.colon = state;
+}
+
+void seven_segment_set_decimal_point(uint8_t digit)
+{
+    display_data.display_digits[digit] |= _BV(7);
+}
+
+void seven_segment_show_hour_minute(uint8_t hour, uint8_t minute)
+{
+    // error checking
+    if (hour < 0)
+    {
+        hour = 0;
+    }
+    else if (hour > 23)
+    {
+        hour = 23;
+    }
+    if (minute < 0)
+    {
+        minute = 0;
+    }
+    else if (minute > 59)
+    {
+        minute = 59;
+    }
+
+    uint8_t digit_indices[4];
+
+    // get hour tens digit
+    digit_indices[0] = hour / 10;
+    // get hour ones digit
+    digit_indices[1] = hour % 10;
+
+    // get minute tens digit
+    digit_indices[2] = minute / 10;
+    // get minute ones digit
+    digit_indices[3] = minute % 10;
+
+    // write to display
+    seven_segment_write_digit_vals(digit_indices);
+}
+
+void seven_segment_show_month_day(uint8_t month, uint8_t day)
+{
+    // error checking
+    if (month < 1)
+    {
+        month = 1;
+    }
+    else if (month > 12)
+    {
+        month = 12;
+    }
+    if (day < 1)
+    {
+        day = 1;
+    }
+    else if (day > 31)
+    {
+        day = 31;
+    }
+
+    uint8_t digit_indices[4];
+
+    // get hour tens digit
+    digit_indices[0] = month / 10;
+    // get hour ones digit
+    digit_indices[1] = month % 10;
+
+    // get minute tens digit
+    digit_indices[2] = day / 10;
+    // get minute ones digit
+    digit_indices[3] = day % 10;
+
+    // write to display
+    seven_segment_write_digit_vals(digit_indices);
+}
+
+void seven_segment_show_year(uint16_t year)
+{
+    if (year < 0)
+    {
+        year = 0;
+    }
+    else if (year > 9999)
+    {
+        year = 9999;
+    }
+    uint8_t digit_indices[4];
+    digit_indices[0] = year / 1000;
+    digit_indices[1] = (year % 1000) / 100;
+    digit_indices[2] = (year % 100) / 10;
+    digit_indices[3] = year % 10;
+    seven_segment_write_digit_vals(digit_indices);
+}
+
+void seven_segment_show_temperature(float temperature_celsius)
+{
+    int16_t temperature_int;
+    // 27.0C
+    if (temperature_celsius < -40.)
+    {
+        temperature_celsius = -40.;
+    }
+    else if (temperature_celsius > 125.)
+    {
+        temperature_celsius = 125.;
+    }
+    uint8_t digit_indices[4];
+    // get tens digit
+    if (temperature_celsius < 0.)
+    {
+        temperature_int = temperature_celsius * -10;
+    }
+    else
+    {
+        temperature_int = temperature_celsius * 10;
+    }
+
+    digit_indices[0] = temperature_int / 100;
+    // get ones digit
+    digit_indices[1] = (temperature_int % 100) / 10;
+
+    // get tenths digit
+    digit_indices[2] = (temperature_int % 10);
+    // ones digit gets decimal point
+    // last digit get C for celsius
+    digit_indices[3] = 0xc;
+    seven_segment_write_digit_vals(digit_indices);
+    seven_segment_set_decimal_point(1);
+    // if temperature sub zero, light up the 0th decimal point
+    if (temperature_celsius < 0.)
+    {
+        seven_segment_set_decimal_point(0);
+    }
+}
+
+void seven_segment_show_humidity(float humidity)
+{
+    // 60.0h
+    if (humidity < 0.)
+    {
+        humidity = 0.;
+    }
+    else if (humidity > 100.)
+    {
+        humidity = 100.;
+    }
+    uint8_t digit_indices[4];
+    // get tens digit
+    int16_t humidity_int = humidity * 10;
+    digit_indices[0] = humidity_int / 100;
+    // get ones digit
+    digit_indices[1] = (humidity_int % 100) / 10;
+
+    // get tenths digit
+    digit_indices[2] = (humidity_int % 10);
+    // ones digit gets decimal point
+    // last digit get C for celsius
+    digit_indices[3] = 0x10;
+    seven_segment_write_digit_vals(digit_indices);
+    seven_segment_set_decimal_point(1);
+}
