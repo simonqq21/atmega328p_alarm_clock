7671626a16f818358fdb1ac62f8741b4e0fa7256
SImon QUe <ques86225@gmail.com>
2026-07-16 15:19:23 +0800

    maiden's prayer

--- alarm_clock/lib_testing/piezo/demo.c
+++ alarm_clock/lib_testing/piezo/demo.c
@@ -141,83 +141,158 @@ int main()
     //     piezo_add_note(&notes[i]);
     // }
 
-    uint16_t note_ms = 120;
+    // uint16_t note_ms = 120;
 
-    // never gonna give you up
+    // // never gonna give you up
+    // Note notes[] = {
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
+    //     {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 2},
+
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 3},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 3},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 3},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 2}, // 17
+
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 2},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 2},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 8}, // 29
+
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
+    //     {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 2}, // 37
+
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
+    //     {.ocr1a_value = OCR1A_NOTE_GS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 2},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 2}, // 46
+
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 2},
+    //     {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 2},
+    //     {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
+    //     {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 8},
+    //     {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 6}, // 59
+
+    // };
+    // // 59 notes
+    // for (int i = 0; i < 59 + 1; i++)
+    // {
+    //     piezo_add_note(&notes[i]);
+    // }
+
+    // the maiden's prayer
+    uint16_t note_ms = 250;
     Note notes[] = {
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
-        {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 2},
+        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G7, .duration = note_ms * 5},
+        {.ocr1a_value = OCR1A_NOTE_F7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_F7, .duration = note_ms * 2},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_D7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_C7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_C7, .duration = note_ms * 6}, // 13
 
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 3},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 3},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 3},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 2}, // 17
+        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_D6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_GS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_D7, .duration = note_ms * 5},
+        {.ocr1a_value = OCR1A_NOTE_C7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_C7, .duration = note_ms * 2},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_GS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G6, .duration = note_ms * 6}, // 14
 
+        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 1},
         {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 2},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 2},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 8}, // 29
+        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G7, .duration = note_ms * 5},
+        {.ocr1a_value = OCR1A_NOTE_F7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_F7, .duration = note_ms * 2},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_D7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_C7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_C7, .duration = note_ms * 6}, // 14
 
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
-        {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 3},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 2}, // 37
+        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_D6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_F6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_GS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_D7, .duration = note_ms * 1}, // 6
 
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms},
-        {.ocr1a_value = OCR1A_NOTE_GS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 2},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 2}, // 46
+        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G7, .duration = note_ms * 1}, // 6
 
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 2},
-        {.ocr1a_value = OCR1A_NOTE_C6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_AS5, .duration = note_ms * 1},
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_GS5, .duration = note_ms * 2},
-        {.ocr1a_value = OCR1A_NOTE_DS6, .duration = note_ms * 4},
-        {.ocr1a_value = OCR1A_NOTE_CS6, .duration = note_ms * 8},
-        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 6}, // 59
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_A6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_AS6, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_G7, .duration = note_ms * 2},
+        {.ocr1a_value = OCR1A_NOTE_F7, .duration = note_ms * 1},
+        {.ocr1a_value = OCR1A_NOTE_DS7, .duration = note_ms * 6}, // 6
+
+        {.ocr1a_value = OCR1A_NOTE_REST, .duration = note_ms * 10}, // 1
 
     };
-    // 59 notes
-    for (int i = 0; i < 59 + 1; i++)
+
+    for (int i = 0; i < 60 + 1; i++)
     {
         piezo_add_note(&notes[i]);
     }
-
-    // the maiden's prayer
 
     piezo_play_sequence();
 
